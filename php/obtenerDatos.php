05b6b178efc21215e13cf338fe7f4d19606924db
enamoya2 <enamoya2@gmail.com>
2016-10-26 15:43:48 +0200

    opcional acabados

--- php/obtenerDatos.php
+++ php/obtenerDatos.php
@@ -5,17 +5,20 @@
   <head>
 	<?php include('../adds/StyleAndMeta.php'); ?>
 	<title>Obtener Datos</title>
-  <script type="text/javascript">
+	<script type="text/javascript">
     function completar(){
       var enc=false;
-      var xmlDoc = document.getElementById("data").contentDocument;
-	  alert(xmlDoc);
+	  var xmlDoc = document.getElementById('datos').contentDocument;
 	  var listaemails=xmlDoc.getElementsByTagName("email");
       var correo = document.getElementById("email").value;
       
+	  var listanombres = xmlDoc.getElementsByTagName("nombre");
 	  var listaap1=xmlDoc.getElementsByTagName("apellido1");
 	  var listaap2=xmlDoc.getElementsByTagName("apellido2");
       var listatelefonos=xmlDoc.getElementsByTagName("telefono");
+	  //alert(listaap1.length + "hola1");
+	  //alert(listaap2.length+ "hola2");
+	  //alert(listatelefonos.length+ "hola3");
 	  for (var i = 0; i < listaemails.length && enc==false; i++) {
         if (correo==listaemails[i].childNodes[0].nodeValue){
 			document.getElementById('name').value = listanombres[i].childNodes[0].nodeValue;
@@ -23,17 +26,20 @@
 			document.getElementById('ap2').value = listaap2[i].childNodes[0].nodeValue;
 			document.getElementById('tlf').value = listatelefonos[i].childNodes[0].nodeValue;
 			enc=true;
-			break;
 		}
        }
         if (!enc){
           alert ('Este correo no está registrado en la UPV/EHU. Introduzca otro');
+		  document.getElementById('name').value = "";
+		  document.getElementById('ap1').value = "";
+		  document.getElementById('ap2').value = "";
+		  document.getElementById('tlf').value = "";
         }
       }
     </script>
   </head>
   <body>
-  <OBJECT id="data" data="../xml/usuarios.xml" type="text/xml" style="display:none">
+ <OBJECT id="datos" data="../xml/usuarios.xml" type="text/xml" style="display:none">
   </OBJECT>
   <div id='page-wrap'>
 	<?php include('../adds/header.php'); ?>
